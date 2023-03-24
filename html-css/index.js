c23a6053dcf7316bddae0e7d2f64e20a46a881b5
anasrul03 <anasrul03@gmail.com>
2023-03-24 11:05:32 +0800

    index.js changes

--- html-css/index.js
+++ html-css/index.js
@@ -1,5 +1,18 @@
 let a = "anas";
+let b = a.toUpperCase();
 
+let favoriteSport = ['Futabol','badminton', 'futsal'];
+
+
+for (let i = 0; i < favoriteSport.length; i++){
+
+    console.log(i +' ' +favoriteSport[i]);
+    while (i = 2){
+
+        console.log("TWo is called!");
+    }
+}
 
 
 console.log(a);
+console.log(b);
