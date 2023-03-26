72ddac649c51a66124ccd399961cd9e6b6811389
anasrul03 <anasrul03@gmail.com>
2023-03-27 06:23:47 +0800

    changes made for creating obj and checkboxes

--- html-css/index.js
+++ html-css/index.js
@@ -1,12 +1,8 @@
 console.log("javascript is connected!");
 
-// const buttonClick = () =>{
-
-// console.log("buttonClickisRun");
-// };
 let todoList = [];
 let renderElement = document.getElementById("render_element");
-// var isChecked = false;
+
 var defaultId = 0;
 
 function buttonClick() {
@@ -14,7 +10,8 @@ function buttonClick() {
   var task = document.getElementById("inputText").value;
   var valueElement = document.createElement("span");
   var divElemnt = document.createElement("div");
-  var displayList = document.getElementById("render_element");
+  var checkbox = document.createElement("input");
+  var space = document.createElement("br");
   var deleteButton = document.createElement("button");
   let userObj = { task: task, id: generatingId, isChecked: false };
 
@@ -27,7 +24,13 @@ function buttonClick() {
   console.log(todoList);
 
   //render elements
-  valueElement.innerText = task;
+  renderElement.appendChild(space);
+
+  renderElement.appendChild(checkbox);
+  checkbox.setAttribute("type", "checkbox");
+
+  // valueElement.innerText = task;
+  valueElement.innerHTML = task;
   renderElement.appendChild(valueElement);
   valueElement.setAttribute("id", generatingId);
   // displayList.appendChild(divElemnt);
@@ -41,35 +44,11 @@ function buttonClick() {
 
     console.log("The id for this task is " + generatingId);
   });
-  // document.body.appendChild(deleteButton);
 
-  // console.log(deleteButton);
-  //delete button
+  document.getElementById("inputText").value = "";
 }
 
-//render TodoList Display
-// function displayTodoList() {
-//   console.log("displayTodoList is called");
-//   const list = document.querySelector(".js-todo-list");
-//   const isChecked = todo.checked ? "done" : "";
-//   const node = document.createElement("li");
-//   node.setAttribute("class", `todo-item ${isChecked}`);
-//   node.setAttribute("data-key", todo.id);
-
-//   node.innerHTML = `
-//     <input id="${todo.id}" type="checkbox"/>
-//     <label for="${todo.id}" class="tick js-tick"></label>
-//     <span>${todo.text}</span>
-//     <button class="delete-todo js-delete-todo">
-//     <svg><use href="#delete-icon"></use></svg>
-//     </button>
-//   `;
-
-//   list.append(node);
-// }
-
-//Delete from Array todoList
-
+//Delete from Array todolist
 function deleteFromList() {
   console.log("deleteButtonClick");
   var getID = document.getElementById("taskisSet").getAttribute("id").value;
@@ -77,19 +56,22 @@ function deleteFromList() {
   console.log(getID + " deleted");
 }
 
-function myFunction() {
-  var x = document.getElementById("inputText").value;
-  document.getElementById("demo").innerHTML = x;
-}
+// function myFunction() {
+//   var x = document.getElementById("inputText").value;
+//   document.getElementById("demo").innerHTML = x;
+// }
 
+//Generate Unique ID Function
 var createID = () => {
   var uniqID = "id" + Math.random().toString(16).slice(2);
 
   console.log("the id for this task is " + uniqID);
   return uniqID;
 };
-// const lalaa = () =>{
 
-//     const input = document.getElementById('nameText').value;
-//     console.log(input);
-// }
+//clear input
+function clearInput(task) {
+  if (task.value == "clear input") {
+    task.value = "";
+  }
+}
